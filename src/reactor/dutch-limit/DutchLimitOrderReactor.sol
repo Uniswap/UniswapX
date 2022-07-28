b0397c5e8cacd4868103c0011ebf1906b27bc8be
AzFlin <azflin@users.noreply.github.com>
2022-07-28 11:31:54 -0400

    continue

--- src/reactor/dutch-limit/DutchLimitOrderReactor.sol
+++ src/reactor/dutch-limit/DutchLimitOrderReactor.sol
@@ -4,21 +4,37 @@ pragma solidity ^0.8.0;
 import {OrderFiller} from "../../lib/OrderFiller.sol";
 import {OrderValidator} from "../../lib/OrderValidator.sol";
 import {DutchLimitOrder, DutchLimitOrderExecution} from "./DutchLimitOrderStructs.sol";
-import {ResolvedOrder, TokenAmount} from "../../interfaces/ReactorStructs.sol";
+import {ResolvedOrder, TokenAmount, Output} from "../../interfaces/ReactorStructs.sol";
 
-/// @notice Reactor for simple limit orders
+/// @notice Reactor for dutch limit orders
 contract DutchLimitOrderReactor is OrderValidator {
     using OrderFiller for ResolvedOrder;
 
     function execute(DutchLimitOrderExecution calldata execution) external {
         validateOrder(execution.order.info);
-        ResolvedOrder memory order =
-        ResolvedOrder(execution.order.input, execution.order.outputs);
+        ResolvedOrder memory order = resolve(execution.order);
         order.fill(
             execution.order.info.offerer,
             execution.sig,
             execution.fillContract,
             execution.fillData
         );
+    }
+
+    function resolve(DutchLimitOrder calldata dutchLimitOrder) public view returns (ResolvedOrder memory resolvedOrder) {
+        Output[] memory outputs = new Output[](dutchLimitOrder.outputs.length);
+        for (uint i = 0; i < outputs.length; i++) {
+            uint decayedAmount =
+                dutchLimitOrder.outputs[i].startAmount
+                - (dutchLimitOrder.outputs[i].startAmount - dutchLimitOrder.outputs[i].endAmount)
+                * (block.timestamp - dutchLimitOrder.startTime)
+                / (dutchLimitOrder.endTime - dutchLimitOrder.startTime);
+            outputs[i] = Output(
+                dutchLimitOrder.outputs[i].token,
+                decayedAmount,
+                dutchLimitOrder.outputs[i].recipient
+            );
+        }
+        resolvedOrder = ResolvedOrder(dutchLimitOrder.input, outputs);
     }
 }
--- src/reactor/dutch-limit/DutchLimitOrderStructs.sol
+++ src/reactor/dutch-limit/DutchLimitOrderStructs.sol
@@ -3,17 +3,23 @@ pragma solidity ^0.8.0;
 
 import {
     TokenAmount,
-    Output,
     OrderInfo,
     Signature
 } from "../../interfaces/ReactorStructs.sol";
+
+struct DutchOutput {
+    address token;
+    uint256 startAmount;
+    uint256 endAmount;
+    address recipient;
+}
 
 struct DutchLimitOrder {
     OrderInfo info;
     uint256 startTime;
     uint256 endTime;
     TokenAmount input;
-    Output[] outputs;
+    DutchOutput[] outputs;
 }
 
 struct DutchLimitOrderExecution {
