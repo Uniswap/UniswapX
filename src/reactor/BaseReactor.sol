508ed38516b894e1732424ec2691a70ad1d6e4a2
Mark Toda <toda.mark@gmail.com>
2022-08-08 15:00:53 -0700

    feat: add order status validation
    This commit adds order status validation, enforcing that orders cannot
    be executed twice and can't be executed at all if they have been
    cancelled by the user.
    
    Also adds BaseReactor contract which includes some shared logic that
    should be run by all reactors on fill. Ordertype-specific reactors
    should only validate the ordertype-specific data and resolve into the
    generic order struct. This way we can ideally run as many critical flows
    through the base reactor.

---
+++ src/reactor/BaseReactor.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {OrderFiller} from "../lib/OrderFiller.sol";
+import {OrderValidator} from "../lib/OrderValidator.sol";
+import {
+    ResolvedOrder,
+    OrderFill,
+    OrderInfo,
+    OrderStatus,
+    TokenAmount
+} from "../interfaces/ReactorStructs.sol";
+
+/// @notice Reactor for simple limit orders
+contract BaseReactor {
+    using OrderFiller for OrderFill;
+    using OrderValidator for mapping(bytes32 => OrderStatus);
+    using OrderValidator for OrderInfo;
+
+    address public immutable permitPost;
+
+    mapping(bytes32 => OrderStatus) public orderStatus;
+
+    constructor(address _permitPost) {
+        permitPost = _permitPost;
+    }
+
+    /// @notice validates and fills an order, marking it as filled
+    function fill(OrderFill memory orderFill) internal {
+        orderFill.order.info.validate();
+        orderStatus.updateFilled(orderFill.orderHash);
+        orderFill.fill();
+    }
+}
