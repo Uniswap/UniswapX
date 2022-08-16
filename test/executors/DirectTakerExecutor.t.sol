8601185f91eeda56d2bbc6a9a03ed0e2c4d534e0
AzFlin <azflin@users.noreply.github.com>
2022-08-16 09:48:24 -0400

    continuie

--- test/executors/DirectTakerExecutor.t.sol
+++ test/executors/DirectTakerExecutor.t.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {DirectTakerExecutor} from "../../src/sample-executors/DirectTakerExecutor.sol";
+import {DutchLimitOrderReactor} from "../../src/reactor/dutch-limit/DutchLimitOrderReactor.sol";
 import {MockERC20} from "../../src/test/MockERC20.sol";
 import {Output} from "../../src/interfaces/ReactorStructs.sol";
+import {PermitPost, Permit} from "permitpost/PermitPost.sol";
 
 contract DirectTakerExecutorTest is Test {
     uint256 takerPrivateKey;
@@ -12,15 +14,26 @@ contract DirectTakerExecutorTest is Test {
     MockERC20 tokenOut;
     address taker;
     DirectTakerExecutor directTakerExecutor;
+    DutchLimitOrderReactor dloReactor;
+    PermitPost permitPost;
 
     uint256 constant ONE = 10 ** 18;
 
     function setUp() public {
+        // Mock input/output tokens
         tokenIn = new MockERC20("Input", "IN", 18);
         tokenOut = new MockERC20("Output", "OUT", 18);
+
+        // Mock taker
         takerPrivateKey = 0x12341234;
         taker = vm.addr(takerPrivateKey);
+
+        // Instantiate relevant contracts
         directTakerExecutor = new DirectTakerExecutor();
+        permitPost = new PermitPost();
+        dloReactor = new DutchLimitOrderReactor(address(permitPost));
+
+        // Give 1 tokenIn to executor, 1 tokenOut to taker
         tokenIn.mint(address(directTakerExecutor), ONE);
         tokenOut.mint(taker, ONE);
         tokenOut.forceApprove(taker, address(directTakerExecutor), ONE);
@@ -30,7 +43,7 @@ contract DirectTakerExecutorTest is Test {
         Output[] memory outputs = new Output[](1);
         outputs[0].token = address(tokenOut);
         outputs[0].amount = ONE;
-        bytes memory fillData = abi.encode(taker, tokenIn, ONE, address(0));
+        bytes memory fillData = abi.encode(taker, tokenIn, ONE, dloReactor);
         directTakerExecutor.reactorCallback(outputs, fillData);
         assertEq(tokenIn.balanceOf(taker), ONE);
         assertEq(tokenOut.balanceOf(address(directTakerExecutor)), ONE);
@@ -44,7 +57,7 @@ contract DirectTakerExecutorTest is Test {
         outputs[0].amount = ONE;
         outputs[1].token = address(tokenOut);
         outputs[1].amount = ONE * 2;
-        bytes memory fillData = abi.encode(taker, tokenIn, ONE, address(0));
+        bytes memory fillData = abi.encode(taker, tokenIn, ONE, dloReactor);
         directTakerExecutor.reactorCallback(outputs, fillData);
         assertEq(tokenIn.balanceOf(taker), ONE);
         assertEq(tokenOut.balanceOf(address(directTakerExecutor)), ONE * 3);
