3413b5c36daf667a3bfa8e050410fd19cc1d516b
Mark Toda <toda.mark@gmail.com>
2022-08-29 11:59:29 -0700

    feat: use muldivdown for dutch decay
    More efficient and resistant to mid-operation overflow

--- src/reactor/dutch-limit/DutchLimitOrderReactor.sol
+++ src/reactor/dutch-limit/DutchLimitOrderReactor.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {BaseReactor} from "../BaseReactor.sol";
 import {DutchLimitOrder, DutchOutput} from "./DutchLimitOrderStructs.sol";
 import {ResolvedOrder, TokenAmount, OrderInfo, Output, Signature} from "../../lib/ReactorStructs.sol";
 
 /// @notice Reactor for dutch limit orders
 contract DutchLimitOrderReactor is BaseReactor {
+    using FixedPointMathLib for uint256;
+
     error EndTimeBeforeStart();
     error DeadlineBeforeEndTime();
+    error NotStarted();
 
     constructor(address _permitPost) BaseReactor(_permitPost) {}
 
@@ -40,16 +44,22 @@ contract DutchLimitOrderReactor is BaseReactor {
     {
         Output[] memory outputs = new Output[](dutchLimitOrder.outputs.length);
         for (uint256 i = 0; i < outputs.length; i++) {
-            DutchOutput calldata dutchOutput_i = dutchLimitOrder.outputs[i];
+            DutchOutput calldata output = dutchLimitOrder.outputs[i];
             uint256 decayedAmount;
-            if (dutchLimitOrder.endTime < block.timestamp) {
-                decayedAmount = dutchOutput_i.endAmount;
+
+            if (dutchLimitOrder.endTime <= block.timestamp || output.startAmount == output.endAmount) {
+                decayedAmount = output.endAmount;
+            } else if (dutchLimitOrder.startTime >= block.timestamp) {
+                decayedAmount = output.startAmount;
             } else {
-                decayedAmount = dutchOutput_i.startAmount
-                    - (dutchOutput_i.startAmount - dutchOutput_i.endAmount) * (block.timestamp - dutchLimitOrder.startTime)
-                        / (dutchLimitOrder.endTime - dutchLimitOrder.startTime);
+                // TODO: maybe handle case where startAmount < endAmount
+                // i.e. for exactOutput case
+                uint256 elapsed = block.timestamp - dutchLimitOrder.startTime;
+                uint256 duration = dutchLimitOrder.endTime - dutchLimitOrder.startTime;
+                uint256 decayAmount = output.startAmount - output.endAmount;
+                decayedAmount = output.startAmount - decayAmount.mulDivDown(elapsed, duration);
             }
-            outputs[i] = Output(dutchOutput_i.token, decayedAmount, dutchOutput_i.recipient);
+            outputs[i] = Output(output.token, decayedAmount, output.recipient);
         }
         resolvedOrder = ResolvedOrder(dutchLimitOrder.info, dutchLimitOrder.input, outputs);
     }
@@ -67,6 +77,7 @@ contract DutchLimitOrderReactor is BaseReactor {
         if (dutchLimitOrder.endTime <= dutchLimitOrder.startTime) {
             revert EndTimeBeforeStart();
         }
+
         if (dutchLimitOrder.info.deadline < dutchLimitOrder.endTime) {
             revert DeadlineBeforeEndTime();
         }
