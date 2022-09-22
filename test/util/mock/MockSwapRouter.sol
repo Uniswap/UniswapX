f69b59f59619fd3f0309eb81f8006e90dd58f9b5
Mark Toda <toda.mark@gmail.com>
2022-09-22 14:11:38 -0700

    feat: improve executors
    This commit adds a few improvements to the sample executors:
    - pass taker address as parameter for validation
    - add ownership for executors so the owner can claim their earnings
      / protect against misuse
    - claim earnings for uniswap v3
    - uniswap v3 takes full path instead of only single pool swaps

--- test/util/mock/MockSwapRouter.sol
+++ test/util/mock/MockSwapRouter.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.8.16;
 
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Path} from "../lib/Path.sol";
 import {IUniV3SwapRouter} from "../../../src/external/IUniV3SwapRouter.sol";
 
 contract MockSwapRouter {
     using SafeTransferLib for ERC20;
+    using Path for bytes;
 
     uint256 public constant SWAP_RATE_GRANULARITY = 10000;
     uint256 public swapRate = 10000; // start with swap at 1 for 1
@@ -14,23 +16,18 @@ contract MockSwapRouter {
         swapRate = newRate;
     }
 
-    function exactOutputSingle(IUniV3SwapRouter.ExactOutputSingleParams calldata params)
-        external
-        returns (uint256 amountIn)
-    {
-        amountIn = (params.amountOut * swapRate) / SWAP_RATE_GRANULARITY;
-        require(amountIn <= params.amountInMaximum, "Too much requested");
-        ERC20(params.tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
-        ERC20(params.tokenOut).safeTransfer(params.recipient, params.amountOut);
-    }
+    function exactInput(IUniV3SwapRouter.ExactInputParams calldata params) external returns (uint256 amountOut) {
+        bytes memory path = params.path;
+        (address tokenIn,,) = path.decodeFirstPool();
 
-    function exactInputSingle(IUniV3SwapRouter.ExactInputSingleParams calldata params)
-        external
-        returns (uint256 amountOut)
-    {
+        while (path.hasMultiplePools()) {
+            path = path.skipToken();
+        }
+        (, address tokenOut,) = path.decodeFirstPool();
+
         amountOut = (params.amountIn * swapRate) / SWAP_RATE_GRANULARITY;
         require(amountOut >= params.amountOutMinimum, "Too little received");
-        ERC20(params.tokenIn).safeTransferFrom(msg.sender, address(this), params.amountIn);
-        ERC20(params.tokenOut).safeTransfer(params.recipient, amountOut);
+        ERC20(tokenIn).safeTransferFrom(msg.sender, address(this), params.amountIn);
+        ERC20(tokenOut).safeTransfer(params.recipient, amountOut);
     }
 }
