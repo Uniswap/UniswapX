e1a8db7a3fa84a7b6710fd89f21b88fc0007344d
Mark Toda <toda.mark@gmail.com>
2022-11-18 15:01:08 -0800

    feat: add basic IPS

---
+++ src/base/IPSFees.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.16;
+
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {ISignatureTransfer} from "permit2/interfaces/ISignatureTransfer.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ReactorEvents} from "../base/ReactorEvents.sol";
+import {OrderInfoLib} from "../lib/OrderInfoLib.sol";
+import {IReactorCallback} from "../interfaces/IReactorCallback.sol";
+import {IReactor} from "../interfaces/IReactor.sol";
+import {SignedOrder, ResolvedOrder, OrderInfo, InputToken, OutputToken} from "../base/ReactorStructs.sol";
+
+/// @notice Handling for interface-protocol-split fees
+abstract contract IPSFees {
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+
+    error InvalidFee();
+    error NoFeeOutput();
+    error UnauthorizedFeeRecipient();
+
+    /// @notice stores the owed fees
+    /// @dev maps token address to owner address to amount
+    mapping(address => mapping(address => uint256)) public feesOwed;
+
+    /// The number of basis points per whole
+    uint256 private constant BPS = 10000;
+
+    /// The amount of fees to take in basis points
+    uint256 public immutable PROTOCOL_FEE_BPS;
+
+    /// The address who can receive fees
+    address public protocolFeeRecipient;
+
+    modifier onlyProtocolFeeRecipient() {
+        if (msg.sender != protocolFeeRecipient) revert UnauthorizedFeeRecipient();
+        _;
+    }
+
+    constructor(uint256 _protocolFeeBps, address _protocolFeeRecipient) {
+        if (_protocolFeeBps > BPS) revert InvalidFee();
+
+        PROTOCOL_FEE_BPS = _protocolFeeBps;
+        // TODO: do we want to separate the setter from the recipient?
+        // i.e. governance is the setter, but can delegate to someone else
+        // and still only governance can re-delegate
+        protocolFeeRecipient = _protocolFeeRecipient;
+    }
+
+    /// @notice Takes fees from the order
+    /// @dev modifies the fee output recipient to be this contract
+    /// @dev stores state to allow fee recipients to claim later
+    /// @dev Note: the fee output is defined as the last output in the order
+    /// @param order The encoded order to take fees from
+    function _takeFees(ResolvedOrder memory order) internal {
+        if (order.outputs.length == 0) revert NoFeeOutput();
+        OutputToken memory feeOutput = order.outputs[order.outputs.length - 1];
+        uint256 protocolFeeAmount = feeOutput.amount.mulDivDown(PROTOCOL_FEE_BPS, BPS);
+
+        // protocol fees are accrued to address[0]
+        feesOwed[feeOutput.token][address(0)] += protocolFeeAmount;
+        // rest goes to the original interface fee recipient
+        feesOwed[feeOutput.token][feeOutput.recipient] += feeOutput.amount - protocolFeeAmount;
+
+        // TODO: consider / bench just adding protocol fee output to the order
+        //  and having filler send directly to both recipients
+        // we custody the fee and they can claim later
+        feeOutput.recipient = address(this);
+    }
+
+    /// @notice claim accrued fees
+    /// @param token The token to claim fees for
+    function claimFees(address token) external {
+        address feeRecipient = msg.sender == protocolFeeRecipient ? address(0) : msg.sender;
+        uint256 amount = feesOwed[token][feeRecipient];
+        feesOwed[token][feeRecipient] = 0;
+        ERC20(token).safeTransfer(msg.sender, amount);
+    }
+
+    /// @notice sets the protocol fee recipient
+    /// @dev only callable by the current protocol fee recipient
+    /// @param _protocolFeeRecipient the new fee recipient
+    function setProtocolFeeRecipient(address _protocolFeeRecipient) external onlyProtocolFeeRecipient {
+        protocolFeeRecipient = _protocolFeeRecipient;
+    }
+}
