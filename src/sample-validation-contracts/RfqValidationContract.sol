e271224e81e2398c7fd54ba1e104075fb254bf23
Allen Lin <allenlin1992@hotmail.com>
2022-12-08 14:55:23 -0500

    write draft

--- src/sample-validation-contracts/RfqValidationContract.sol
+++ src/sample-validation-contracts/RfqValidationContract.sol
@@ -5,6 +5,7 @@ import {IValidationCallback} from "../interfaces/IValidationCallback.sol";
 import {ResolvedOrder, OrderInfo} from "../base/ReactorStructs.sol";
 
 contract RfqValidationContract is IValidationCallback {
+    error InvalidFiller();
 
     constructor() {}
 
@@ -13,6 +14,10 @@ contract RfqValidationContract is IValidationCallback {
         view
         returns (bool)
     {
-
+        (address exclusiveFiller, uint256 lastExclusiveTimestamp) = abi.decode(order.validationData, (address, uint256));
+        if (block.timestamp <= lastExclusiveTimestamp && filler != exclusiveFiller) {
+            return false;
+        }
+        return true;
     }
 }
