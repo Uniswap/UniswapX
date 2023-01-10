d2b2ad67c592a333f270a9c1c16499234beb1204
Emily Williams <emag3m@gmail.com>
2023-01-10 17:59:49 -0500

    import permit2 interfaces

--- test/util/mock/MockERC20.sol
+++ test/util/mock/MockERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.16;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol, decimals) {}
