1297366fb9fb68415bd9eff5b675b7c43d72aa1c
Emily Williams <emag3m@gmail.com>
2023-01-12 15:24:32 -0500

    begin cross-chain skeleton

--- src/xchain-gouda/base/XReactorStructs.sol
+++ src/xchain-gouda/base/XReactorStructs.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.16;
 
-/// @dev generic order information
-///  should be included as the first field in any concrete order types
-struct OrderInfo {
+import {InputToken} from "../../base/ReactorStructs.sol";
+
+/// @dev generic cross-chain order information
+///  should be included as the first field in any concrete cross-chain order types
+struct XOrderInfo {
     // The address of the reactor that this order is targeting
     // Note that this must be included in every order so the offerer
     // signature commits to the specific reactor that they trust to fill their order properly
@@ -13,42 +15,38 @@ struct OrderInfo {
     address offerer;
     // The nonce of the order, allowing for signature replay protection and cancellation
     uint256 nonce;
-    // The timestamp after which this order is no longer valid
-    uint256 deadline;
+    // The timestamp after which this order is no longer valid to initiateSettlement
+    uint256 fillDeadline;
+    // The timestamp after which the order may be cancelled if it has not been settled
+    uint256 settlementDeadline;
+    // The address of the oracle that determines whether the order was sucessfully carried
+    // out on the output chain
+    address settlementOracle;
     // Custom validation contract
     address validationContract;
     // Encoded validation params for validationContract
     bytes validationData;
 }
 
-/// @dev tokens that need to be sent from the offerer in order to satisfy an order
-struct InputToken {
-    address token;
-    uint256 amount;
-    // Needed for dutch decaying inputs
-    uint256 maxAmount;
+struct XCollateralToken {
+  address token;
+  uint256 amount;
 }
 
 /// @dev tokens that need to be received by the recipient on another chain in order to satisfy an order
-struct OutputToken {
+struct XOutputToken {
     address token;
     uint256 amount;
     address recipient;
-    bool isFeeOutput;
+    uint256 chainId;
 }
 
-/// @dev generic concrete order that specifies exact tokens which need to be sent and received
-struct ResolvedOrder {
-    OrderInfo info;
+/// @dev generic concrete cross-chain order that specifies exact tokens which need to be sent and received
+struct ResolvedXOrder {
+    XOrderInfo info;
     InputToken input;
-    OutputToken[] outputs;
+    XCollateralToken collateral;
+    XOutputToken[] outputs;
     bytes sig;
     bytes32 hash;
-}
-
-/// @dev external struct including a generic encoded order and offerer signature
-///  The order bytes will be parsed and mapped to a ResolvedOrder in the concrete reactor contract
-struct SignedOrder {
-    bytes order;
-    bytes sig;
 }
