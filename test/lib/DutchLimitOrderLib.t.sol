ceee516d54c6942c85340b8d16f55448e6ffe8ca
Allen Lin <allenlin1992@hotmail.com>
2023-01-13 12:56:02 -0500

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/lib/DutchLimitOrderLib.t.sol
+++ test/lib/DutchLimitOrderLib.t.sol
@@ -160,4 +160,24 @@ contract DutchLimitOrderLibTest is Test {
         });
         assertTrue(order1.hash() != order2.hash());
     }
+
+    function testHashChangesWhenEndTimeChanges(uint256 endTimeAdjustment) public {
+        vm.assume(endTimeAdjustment != 100);
+        vm.assume(endTimeAdjustment < 100000000);
+        DutchLimitOrder memory order1 = DutchLimitOrder({
+            info: OrderInfoBuilder.init(REACTOR).withOfferer(MAKER).withDeadline(block.timestamp + 100),
+            startTime: block.timestamp - 100,
+            endTime: block.timestamp + 100,
+            input: DutchInput(TOKEN_IN, ONE, ONE),
+            outputs: OutputsBuilder.singleDutch(TOKEN_OUT, ONE, 0, MAKER)
+        });
+        DutchLimitOrder memory order2 = DutchLimitOrder({
+            info: OrderInfoBuilder.init(REACTOR).withOfferer(MAKER).withDeadline(block.timestamp + 100),
+            startTime: block.timestamp - 100,
+            endTime: block.timestamp + endTimeAdjustment,
+            input: DutchInput(TOKEN_IN, ONE, ONE),
+            outputs: OutputsBuilder.singleDutch(TOKEN_OUT, ONE, 0, MAKER)
+        });
+        assertTrue(order1.hash() != order2.hash());
+    }
 }
