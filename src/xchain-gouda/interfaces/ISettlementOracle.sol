24ba1c5cf58a14416560716b512aa73d0aac1311
Emily Williams <emag3m@gmail.com>
2023-01-17 14:04:04 -0500

    renaming

--- src/xchain-gouda/interfaces/ISettlementOracle.sol
+++ src/xchain-gouda/interfaces/ISettlementOracle.sol
@@ -4,17 +4,24 @@ pragma solidity ^0.8.16;
 import {ResolvedOrder} from "../base/SettlementStructs.sol";
 import {SignedOrder} from "../../base/ReactorStructs.sol";
 
-/// @notice Interface for cross-chain order execution reactors
+struct SettlementFillInfo {
+    uint32 chainId;
+    address recipient;
+    address token;
+    uint256 amount;
+}
+
+/// @notice Interface for cross chain listeners for gouda
 interface ISettlementOracle {
-    /// @notice Initiate a single order using the given fill specification
-    /// @param order The cross-chain order definition and valid signature to execute
-    function initiateSettlement(SignedOrder calldata order, address fillRecipient) external;
+    /// @notice Get the settlementInfo associated with an orderId.
+    /// @param orderId The cross-chain orderId
+    /// @return settlementInfo The settlmentInfo that was passed to the cross-chain listener from a valid source
+    function getSettlementFillInfo(bytes32 orderId) external view returns (SettlementFillInfo[] calldata);
 
-    /// @notice Execute the given orders at once with the specified fill specification
-    /// @param settlementId The id that identifies the current settlement in progress
-    function finalizeSettlement(bytes32 settlementId) external;
-
-    /// @notice Cancels a settmentlent that was never filled after the settlement deadline
-    /// @param settlementId The id that identifies the settlement to cancel
-    function cancelSettlement(bytes32 settlementId) external;
+    /// @notice Logs the settlement info given for an orderId
+    /// @dev Access to this function must be restricted to valid message bridges and must verify that chainId corresponds
+    /// to the correct bridge.
+    /// @param settlementInfo The settlmentInfo that should be logged for a given orderId
+    /// @param orderId The cross-chain orderId
+    function logSettlementFillInfo(SettlementFillInfo[] calldata settlementInfo, bytes32 orderId) external;
 }
