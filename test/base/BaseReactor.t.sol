6e25f2147f3b409841c1314eb28eff7b34ede84f
Eric Zhong <eric.zhong@uniswap.org>
2023-01-31 14:05:06 -0500

    Revisions, and add ArrayBuilder to create 1d and 2d arays easier

--- test/base/BaseReactor.t.sol
+++ test/base/BaseReactor.t.sol
@@ -9,11 +9,14 @@ import {ReactorEvents} from "../../src/base/ReactorEvents.sol";
 import {SignedOrder, OrderInfo, InputToken, OutputToken} from "../../src/base/ReactorStructs.sol";
 import {DeployPermit2} from "../util/DeployPermit2.sol";
 import {OrderInfoBuilder} from "../util/OrderInfoBuilder.sol";
+import {ArrayBuilder} from "../util/ArrayBuilder.sol";
 import {MockERC20} from "../util/mock/MockERC20.sol";
 import {MockFillContract} from "../util/mock/MockFillContract.sol";
 
 abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
     using OrderInfoBuilder for OrderInfo;
+    using ArrayBuilder for uint256[];
+    using ArrayBuilder for uint256[][];
 
     uint256 constant ONE = 10 ** 18;
 
@@ -66,13 +69,15 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
             OrderInfoBuilder.init(address(reactor)).withOfferer(maker).withDeadline(block.timestamp + 100);
         (SignedOrder memory signedOrder, bytes32 orderHash) = createAndSignOrder(orderInfo, inputAmount, outputAmount);
 
-        uint256 makerInputBalanceStart = tokenIn.balanceOf(address(maker));
-        uint256 fillContractInputBalanceStart = tokenIn.balanceOf(address(fillContract));
-        uint256 makerOutputBalanceStart = tokenOut.balanceOf(address(maker));
-        uint256 fillContractOutputBalanceStart = tokenOut.balanceOf(address(fillContract));
+        (
+            uint256 makerInputBalanceStart,
+            uint256 fillContractInputBalanceStart,
+            uint256 makerOutputBalanceStart,
+            uint256 fillContractOutputBalanceStart
+        ) = _checkpointBalances();
 
         // TODO: expand to allow for custom fillData in 3rd param
-        vm.expectEmit(false, false, false, true, address(reactor));
+        vm.expectEmit(true, true, true, true, address(reactor));
         emit Fill(orderHash, address(this), maker, orderInfo.nonce);
         // execute order
         snapStart(string.concat(name(), "BaseExecuteSingle"));
@@ -86,7 +91,7 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
     }
 
     /// @dev Basic batch execute test
-    // Two orders: 1. inputs = 1, outputs = 2, 2. inputs = 2, outputs = 4
+    // Two orders: (inputs = 1, outputs = 2), (inputs = 2, outputs = 4)
     function testBaseExecuteBatch() public {
         uint256 inputAmount = ONE;
         uint256 outputAmount = 2 * inputAmount;
@@ -95,29 +100,13 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
         tokenOut.mint(address(fillContract), 6 * 10 ** 18);
         tokenIn.forceApprove(maker, address(permit2), type(uint256).max);
 
-        uint256[] memory inputAmounts = new uint256[](2);
-        inputAmounts[0] = inputAmount;
-        inputAmounts[1] = 2 * inputAmount;
+        uint256[] memory inputAmounts = ArrayBuilder.fill(1, inputAmount).push(2 * inputAmount);
+        uint256[][] memory outputAmounts = ArrayBuilder.initNested(2).set(0, ArrayBuilder.fill(1, outputAmount)).set(
+            1, ArrayBuilder.fill(1, 2 * outputAmount)
+        );
 
-        uint256[][] memory outputAmounts = new uint256[][](2);
-        uint256[] memory o1 = new uint256[](1);
-        uint256[] memory o2 = new uint256[](1);
-        o1[0] = outputAmount;
-        o2[0] = 2 * outputAmount;
-        outputAmounts[0] = o1;
-        outputAmounts[1] = o2;
-
-        // inefficient, but makes the balance check clear
-        uint256 totalOutputAmount;
-        for (uint256 i = 0; i < outputAmounts.length; i++) {
-            for (uint256 j = 0; j < outputAmounts[i].length; j++) {
-                totalOutputAmount += outputAmounts[i][j];
-            }
-        }
-        uint256 totalInputAmount;
-        for (uint256 i = 0; i < inputAmounts.length; i++) {
-            totalInputAmount += inputAmounts[i];
-        }
+        uint256 totalOutputAmount = outputAmounts.sum();
+        uint256 totalInputAmount = inputAmounts.sum();
 
         OrderInfo[] memory infos = new OrderInfo[](2);
         infos[0] =
@@ -127,9 +116,9 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
 
         (SignedOrder[] memory signedOrders, bytes32[] memory orderHashes) =
             createAndSignBatchOrders(infos, inputAmounts, outputAmounts);
-        vm.expectEmit(false, false, false, true);
+        vm.expectEmit(true, true, true, true);
         emit Fill(orderHashes[0], address(this), maker, infos[0].nonce);
-        vm.expectEmit(false, false, false, true);
+        vm.expectEmit(true, true, true, true);
         emit Fill(orderHashes[1], address(this), maker, infos[1].nonce);
 
         snapStart(string.concat(name(), "BaseExecuteBatch"));
@@ -153,9 +142,21 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
             OrderInfoBuilder.init(address(reactor)).withOfferer(maker).withDeadline(block.timestamp + 100);
         (SignedOrder memory signedOrder, bytes32 orderHash) = createAndSignOrder(orderInfo, inputAmount, outputAmount);
 
-        vm.expectEmit(false, false, false, true, address(reactor));
+        (
+            uint256 makerInputBalanceStart,
+            uint256 fillContractInputBalanceStart,
+            uint256 makerOutputBalanceStart,
+            uint256 fillContractOutputBalanceStart
+        ) = _checkpointBalances();
+
+        vm.expectEmit(true, true, true, true, address(reactor));
         emit Fill(orderHash, address(this), maker, orderInfo.nonce);
         reactor.execute(signedOrder, address(fillContract), bytes(""));
+
+        assertEq(tokenIn.balanceOf(address(maker)), makerInputBalanceStart - inputAmount);
+        assertEq(tokenIn.balanceOf(address(fillContract)), fillContractInputBalanceStart + inputAmount);
+        assertEq(tokenOut.balanceOf(address(maker)), makerOutputBalanceStart + outputAmount);
+        assertEq(tokenOut.balanceOf(address(fillContract)), fillContractOutputBalanceStart - outputAmount);
 
         tokenIn.mint(address(maker), inputAmount);
         tokenOut.mint(address(fillContract), outputAmount);
@@ -188,14 +189,42 @@ abstract contract BaseReactorTest is GasSnapshot, ReactorEvents, Test {
         ).withNonce(123);
         (SignedOrder memory signedOrder, bytes32 orderHash) = createAndSignOrder(orderInfo, inputAmount, outputAmount);
 
-        vm.expectEmit(false, false, false, true, address(reactor));
+        (
+            uint256 makerInputBalanceStart,
+            uint256 fillContractInputBalanceStart,
+            uint256 makerOutputBalanceStart,
+            uint256 fillContractOutputBalanceStart
+        ) = _checkpointBalances();
+
+        vm.expectEmit(true, true, true, true, address(reactor));
         emit Fill(orderHash, address(this), maker, orderInfo.nonce);
         reactor.execute(signedOrder, address(fillContract), bytes(""));
+
+        assertEq(tokenIn.balanceOf(address(maker)), makerInputBalanceStart - inputAmount);
+        assertEq(tokenIn.balanceOf(address(fillContract)), fillContractInputBalanceStart + inputAmount);
+        assertEq(tokenOut.balanceOf(address(maker)), makerOutputBalanceStart + outputAmount);
+        assertEq(tokenOut.balanceOf(address(fillContract)), fillContractOutputBalanceStart - outputAmount);
 
         orderInfo = OrderInfoBuilder.init(address(reactor)).withOfferer(maker).withDeadline(block.timestamp + 100)
             .withNonce(123);
         (signedOrder, orderHash) = createAndSignOrder(orderInfo, inputAmount, outputAmount);
         vm.expectRevert(InvalidNonce.selector);
         reactor.execute(signedOrder, address(fillContract), bytes(""));
+    }
+
+    function _checkpointBalances()
+        internal
+        view
+        returns (
+            uint256 makerInputBalanceStart,
+            uint256 fillContractInputBalanceStart,
+            uint256 makerOutputBalanceStart,
+            uint256 fillContractOutputBalanceStart
+        )
+    {
+        makerInputBalanceStart = tokenIn.balanceOf(address(maker));
+        fillContractInputBalanceStart = tokenIn.balanceOf(address(fillContract));
+        makerOutputBalanceStart = tokenOut.balanceOf(address(maker));
+        fillContractOutputBalanceStart = tokenOut.balanceOf(address(fillContract));
     }
 }
