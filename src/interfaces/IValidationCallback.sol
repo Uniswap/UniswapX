2ef55a669bf89fd177aaf4a59978238178f43fbe
Allen Lin <allenlin1992@hotmail.com>
2023-02-07 11:46:40 -0500

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- src/interfaces/IValidationCallback.sol
+++ src/interfaces/IValidationCallback.sol
@@ -6,9 +6,10 @@ import {OrderInfo, ResolvedOrder} from "../base/ReactorStructs.sol";
 /// @notice Callback to validate an order
 interface IValidationCallback {
     /// @notice Called by the reactor for custom validation of an order. Will revert with `ValidationFailed()` if
-    /// custom validation fails
+    /// custom validation fails. If this function doesn't revert, then that means the order is valid.
     /// @param filler The filler of the order
     /// @param resolvedOrder The resolved order to fill
-    /// @return the number of basis points that output amounts will increase by
+    /// @return the number of basis points that output amounts will increase by. If 0, then output amounts
+    /// will not increase.
     function validate(address filler, ResolvedOrder calldata resolvedOrder) external view returns (uint256);
 }
