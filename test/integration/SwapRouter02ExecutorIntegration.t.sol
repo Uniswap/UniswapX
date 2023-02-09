015ea14a08da074e6446344dd279904c02c2b142
Allen Lin <allenlin1992@hotmail.com>
2023-02-09 11:54:53 -0500

    continue
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/integration/SwapRouter02ExecutorIntegration.t.sol
+++ test/integration/SwapRouter02ExecutorIntegration.t.sol
@@ -37,43 +37,63 @@ contract SwapRouter02IntegrationTest is Test, PermitSignature {
         vm.prank(maker);
         ERC20(WETH).approve(PERMIT2, type(uint256).max);
 
-        // Transfer 2 WETH to maker
+        // Transfer 3 WETH to maker
         vm.prank(0xF04a5cC80B1E94C69B48f5ee68a08CD2F09A7c3E);
-        ERC20(WETH).transfer(maker, 2 * ONE);
+        ERC20(WETH).transfer(maker, 3 * ONE);
     }
 
-    // Maker's order consists of input = 2 WETH & output = 3000 DAI. There will be 288797467469336654155
-    // excess wei of DAI in SwapRouter02Executor.
-    function testSwap2WethToDaiViaV3() public {
-        uint256 inputAmount = 2 * ONE;
-
-        DutchLimitOrder memory order = DutchLimitOrder({
+    // Maker creates below 2 orders, and both are filled via SwapRouter02Executor via Uniswap V3.
+    // Order 1: input = 2 WETH, output = 3000 DAI
+    // Order 2: input = 1 WETH, output = 1600 DAI
+    // I chose to test using 2 orders to test that the 2nd execute call will not have to pass in
+    // `tokensToApproveForSwapRouter02` nor `tokensToApproveForReactor`.
+    // There will be 288797467469336654155 wei of DAI in SwapRouter02Executor after the 1st order is filled.
+    // There will be 332868886072663242927 wei of DAI in SwapRouter02Executor after the 2nd order is filled.
+    function testSwapWethToDaiViaV3() public {
+        DutchLimitOrder memory order1 = DutchLimitOrder({
             info: OrderInfoBuilder.init(address(dloReactor)).withOfferer(maker).withDeadline(block.timestamp + 100),
             startTime: block.timestamp - 100,
             endTime: block.timestamp + 100,
-            input: DutchInput(address(WETH), inputAmount, inputAmount),
+            input: DutchInput(address(WETH), 2 * ONE, 2 * ONE),
             outputs: OutputsBuilder.singleDutch(address(DAI), 3000 * ONE, 3000 * ONE, address(maker))
+        });
+        DutchLimitOrder memory order2 = DutchLimitOrder({
+            info: OrderInfoBuilder.init(address(dloReactor)).withOfferer(maker).withDeadline(block.timestamp + 100)
+                .withNonce(1),
+            startTime: block.timestamp - 100,
+            endTime: block.timestamp + 100,
+            input: DutchInput(address(WETH), ONE, ONE),
+            outputs: OutputsBuilder.singleDutch(address(DAI), 1600 * ONE, 1600 * ONE, address(maker))
         });
         address[] memory tokensToApproveForSwapRouter02 = new address[](1);
         tokensToApproveForSwapRouter02[0] = WETH;
         address[] memory tokensToApproveForReactor = new address[](1);
         tokensToApproveForReactor[0] = DAI;
-        bytes[] memory multicallData = new bytes[](1);
+        bytes[] memory multicallData1 = new bytes[](1);
+        bytes[] memory multicallData2 = new bytes[](1);
 
-        ExactInputSingleParams memory params =
+        ExactInputSingleParams memory params1 =
             ExactInputSingleParams(WETH, DAI, 500, address(swapRouter02Executor), 2 * ONE, 3000 * ONE, 0);
-        multicallData[0] = abi.encodeWithSelector(ISwapRouter02.exactInputSingle.selector, params);
-
-        assertEq(ERC20(WETH).balanceOf(maker), 2 * ONE);
-        assertEq(ERC20(DAI).balanceOf(maker), 0);
-        assertEq(ERC20(WETH).balanceOf(address(swapRouter02Executor)), 0);
+        multicallData1[0] = abi.encodeWithSelector(ISwapRouter02.exactInputSingle.selector, params1);
         dloReactor.execute(
-            SignedOrder(abi.encode(order), signOrder(makerPrivateKey, PERMIT2, order)),
+            SignedOrder(abi.encode(order1), signOrder(makerPrivateKey, PERMIT2, order1)),
             address(swapRouter02Executor),
-            abi.encode(tokensToApproveForSwapRouter02, tokensToApproveForReactor, multicallData)
+            abi.encode(tokensToApproveForSwapRouter02, tokensToApproveForReactor, multicallData1)
         );
-        assertEq(ERC20(WETH).balanceOf(maker), 0);
+        assertEq(ERC20(WETH).balanceOf(maker), ONE);
         assertEq(ERC20(DAI).balanceOf(maker), 3000 * ONE);
         assertEq(ERC20(DAI).balanceOf(address(swapRouter02Executor)), 288797467469336654155);
+
+        ExactInputSingleParams memory params2 =
+            ExactInputSingleParams(WETH, DAI, 500, address(swapRouter02Executor), ONE, 1600 * ONE, 0);
+        multicallData2[0] = abi.encodeWithSelector(ISwapRouter02.exactInputSingle.selector, params2);
+        dloReactor.execute(
+            SignedOrder(abi.encode(order2), signOrder(makerPrivateKey, PERMIT2, order2)),
+            address(swapRouter02Executor),
+            abi.encode(new address[](0), new address[](0), multicallData2)
+        );
+        assertEq(ERC20(WETH).balanceOf(maker), 0);
+        assertEq(ERC20(DAI).balanceOf(maker), 4600 * ONE);
+        assertEq(ERC20(DAI).balanceOf(address(swapRouter02Executor)), 332868886072663242927);
     }
 }
