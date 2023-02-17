6245792812685f3d34cdcb7f0825e9eaa04f3b5e
AzFlin <allenlin1992@hotmail.com>
2023-02-17 12:05:09 -0500

    Swaprouter02 executor (#102)
    * start
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue (broken)
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * testNotWhitelistedCaller
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * comment
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * testSwapWethToDaiViaV2InsufficientOutput
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * write multicall
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * comment
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * testMulticallOnlyOwner
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * comment
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * comment
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * remove accidental extra param
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * start unit tests (broken)
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * testExecuteOneOrderWithMultipleOutputs
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * continue
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * remove unneccessary if check
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * remove unneccessary if check
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * marks nitty spacing
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    ---------
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

---
+++ src/external/ISwapRouter02.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.16;
+
+struct ExactInputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint24 fee;
+    address recipient;
+    uint256 amountIn;
+    uint256 amountOutMinimum;
+    uint160 sqrtPriceLimitX96;
+}
+
+interface ISwapRouter02 {
+    function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
+    function multicall(uint256 deadline, bytes[] calldata data) external payable returns (bytes[] memory results);
+    function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to)
+        external
+        payable
+        returns (uint256 amountOut);
+    function unwrapWETH9(uint256 amountMinimum) external payable;
+}
--- test/util/mock/MockSwapRouter.sol
+++ test/util/mock/MockSwapRouter.sol
@@ -30,4 +30,22 @@ contract MockSwapRouter {
         ERC20(tokenIn).safeTransferFrom(msg.sender, address(this), params.amountIn);
         ERC20(tokenOut).safeTransfer(params.recipient, amountOut);
     }
+
+    function multicall(uint256 deadline, bytes[] calldata data) public payable returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+            if (!success) {
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577
+                if (result.length < 68) revert();
+                assembly {
+                    result := add(result, 0x04)
+                }
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
+        }
+    }
 }
