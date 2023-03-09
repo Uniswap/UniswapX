16a9d3d384dff9b11ede62c7c24e7a3e1dd88198
Emily Williams <emag3m@gmail.com>
2023-03-09 17:18:35 -0500

    BaseSettlementOracle

---
+++ src/xchain-gouda/oracles/BaseSettlementOracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.16;
+
+import {ISettlementOracle} from "../interfaces/ISettlementOracle.sol";
+import {IOrderSettler} from "../interfaces/IOrderSettler.sol";
+import {OutputToken, SettlementKey} from "../base/SettlementStructs.sol";
+
+/// @notice Generic cross-chain filler logic for filling an order on the target chain
+abstract contract BaseSettlementOracle is ISettlementOracle {
+
+    /// @inheritdoc ISettlementOracle
+    function finalizeSettlement(bytes32 orderId, SettlementKey memory key, address settler, uint256 fillTimestamp)
+        external
+    {
+        authenticateMessageOrigin();
+        IOrderSettler(settler).finalize(orderId, key, fillTimestamp);
+    }
+
+    /// @notice verifies that the cross chain message came from a legitimate source
+    /// @dev this function must revert if msg.sender is not valid bridge contract or if the message was initiated
+    /// from a contract on the target chain that is not a valid SettlementFiller
+    function authenticateMessageOrigin() internal virtual;
+}
