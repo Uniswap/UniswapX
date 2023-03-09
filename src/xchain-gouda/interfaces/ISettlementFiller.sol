14ce0ec0cc3699e5c5cbefbf3e95f91dbaecda59
Emily Williams <emag3m@gmail.com>
2023-03-09 16:11:15 -0500

    check deadline on fill, since its now available in key

--- src/xchain-gouda/interfaces/ISettlementFiller.sol
+++ src/xchain-gouda/interfaces/ISettlementFiller.sol
@@ -11,6 +11,9 @@ interface ISettlementFiller {
     /// @notice Thrown when the hash of the outputs array does not match the outputsHash in the SettlementKey
     error InvalidOutputsHash();
 
+    /// @notice Thrown when trying to fill an order which has a fill deadline that has already passed
+    error FillDeadlineMissed();
+
     /// @notice Thrown when output token does not match the chain id of this deployed contract
     /// @param chainId The invalid chainID
     error InvalidChainId(uint256 chainId);
@@ -18,8 +21,7 @@ interface ISettlementFiller {
     /// @notice Fills an order on the target chain and transmits a message to the origin chain about the details of the
     /// fulfillment including the orderId and the outputs
     /// @dev This function must call the valid bridge to transmit the orderId, settlementKey, and outputs to the origin chain. This function should revert
-    /// if the hash of outputs do not match the outputsHash in they settlement key or if the msg.sender does not match the targetChainFiller in the key.
-    /// @param orderId The cross-chain orderId
+    /// if the hash of outputs do not match the outputsHash in they settlement key.
     /// @param settler The settler contract that holds the settlement, so the bridged message can call finalize on the correct contract.
     /// @param outputs The outputs associated with the corresponding settlement. The outputs MUST be in the same order
     /// as they are in the original order so when hashed they will match the hash in the SettlementKey
