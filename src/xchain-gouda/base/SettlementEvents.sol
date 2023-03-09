8ab5b3c0d42c67d22bb0fa1b039ad12690d99fd8
Emily Williams <emag3m@gmail.com>
2023-03-09 15:48:23 -0500

    cleanup + added comments

--- src/xchain-gouda/base/SettlementEvents.sol
+++ src/xchain-gouda/base/SettlementEvents.sol
@@ -8,7 +8,7 @@ import {CollateralToken} from "./SettlementStructs.sol";
 /// @dev collated into one library to help with forge expectEmit integration
 /// @dev and for reactors which dont use base
 contract SettlementEvents {
-    /// @notice emitted when a settlement is initiated
+    /// @notice emitted when a settlement is initiated. Has all the information needed to construct the associated SettlementKey
     /// @param orderHash The hash of the order to be filled
     /// @param offerer The offerer of the filled order
     /// @param originChainFiller The address that initiates the settlement and recieves input tokens once the order is filled
@@ -16,6 +16,10 @@ contract SettlementEvents {
     /// @param settlementOracle The settlementOracle to be used to determine fulfillment of order
     /// @param optimisticDeadline The timestamp starting at which the settlement may be cancelled if not filled
     /// @param challengeDeadline The timestamp starting at which the settlement may be cancelled if not filled
+    /// @param input The InputToken information associated with the order
+    /// @param fillerCollateral The CollateralToken information associated the filler collateral
+    /// @param challengerCollateral The CollateralToken information associated the challenger collateral
+    /// @param outputsHash A hash of the array of OutputToken parameters associated with the order
     event InitiateSettlement(
         bytes32 indexed orderHash,
         address indexed offerer,
@@ -32,11 +36,15 @@ contract SettlementEvents {
     );
 
     /// @notice emitted when a settlement has been filled successfully
+    /// @param orderId The hash of the order to be finalized 
     event FinalizeSettlement(bytes32 indexed orderId);
 
     /// @notice emitted when a settlement has been cancelled
+    /// @param orderId The hash of the order to be cancelled
     event CancelSettlement(bytes32 indexed orderId);
 
     /// @notice emitted when a settlement has been challenged
+    /// @param orderId The hash of the order to be challenged
+    /// @param challenger The address of the account that has challenged the settlement fill
     event SettlementChallenged(bytes32 indexed orderId, address indexed challenger);
 }
--- src/xchain-gouda/interfaces/ISettlementFiller.sol
+++ src/xchain-gouda/interfaces/ISettlementFiller.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.16;
 
-import {OutputToken} from "../base/SettlementStructs.sol";
+import {OutputToken, SettlementKey} from "../base/SettlementStructs.sol";
 import {SignedOrder} from "../../base/ReactorStructs.sol";
 
 /// @notice Interface for target chain fillers for gouda. A SettlementFiller exists on the target chain, and carries out
 /// transfers from the targetChainFiller to the output recipients in order to verify the stated outputs were filled successfully.
 /// A SettlementFiller is associated with the swapper-selected SettlementOracle on the origin chain.
 interface ISettlementFiller {
+    /// @notice Thrown when the hash of the outputs array does not match the outputsHash in the SettlementKey
+    error InvalidOutputsHash();
+
+    /// @notice Thrown when output token does not match the chain id of this deployed contract
+    /// @param chainId The invalid chainID
+    error InvalidChainId(uint256 chainId);
+
     /// @notice Fills an order on the target chain and transmits a message to the origin chain about the details of the
     /// fulfillment including the orderId and the outputs
-    /// @dev This function must call the valid bridge to transmit the orderId, msg.sender, and outputs to the origin chain
+    /// @dev This function must call the valid bridge to transmit the orderId, settlementKey, and outputs to the origin chain. This function should revert
+    /// if the hash of outputs do not match the outputsHash in they settlement key or if the msg.sender does not match the targetChainFiller in the key.
     /// @param orderId The cross-chain orderId
+    /// @param settler The settler contract that holds the settlement, so the bridged message can call finalize on the correct contract.
     /// @param outputs The outputs associated with the corresponding settlement. The outputs MUST be in the same order
-    /// as they are in the original order, or else the order will not resolve as filled.
-    function fillAndTransmitSettlementOutputs(bytes32 orderId, OutputToken[] calldata outputs) external;
+    /// as they are in the original order so when hashed they will match the hash in the SettlementKey
+    function fillAndTransmitSettlementOutputs(bytes32 orderId, SettlementKey memory key, address settler, OutputToken[] calldata outputs) external;
 }
