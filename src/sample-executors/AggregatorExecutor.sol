f7b1d87f86f4f1752f878bf420a28c0a660e70a2
Sara Reynolds <snreynolds2506@gmail.com>
2023-03-19 22:31:51 -0400

    cleanup and add unwrapWETH

--- src/sample-executors/AggregatorExecutor.sol
+++ src/sample-executors/AggregatorExecutor.sol
@@ -7,6 +7,7 @@ import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import {IReactorCallback} from "../interfaces/IReactorCallback.sol";
 import {ResolvedOrder} from "../base/ReactorStructs.sol";
 import {ISwapRouter02} from "../external/ISwapRouter02.sol";
+import {WETH} from "solmate/src/tokens/WETH.sol";
 
 /// @notice A fill contract that uses the 1inch aggregator to execute trades
 contract AggregatorExecutor is IReactorCallback, Owned {
@@ -15,11 +16,14 @@ contract AggregatorExecutor is IReactorCallback, Owned {
     error SwapFailed();
     error CallerNotWhitelisted();
     error MsgSenderNotReactor();
+    error InsufficientWETHBalance();
 
     address private immutable aggregator;
     address private immutable whitelistedCaller;
     address private immutable reactor;
-    address private immutable swapRouter02;
+
+    ISwapRouter02 private immutable SWAP_ROUTER_02;
+    WETH private immutable WETH9;
 
     constructor(
         address _whitelistedCaller,
@@ -31,15 +35,17 @@ contract AggregatorExecutor is IReactorCallback, Owned {
         whitelistedCaller = _whitelistedCaller;
         reactor = _reactor;
         aggregator = _aggregator;
-        swapRouter02 = _swapRouter02;
+
+        SWAP_ROUTER_02 = ISwapRouter02(_swapRouter02);
+        WETH9 = WETH(payable(SWAP_ROUTER_02.WETH9()));
     }
 
     /// @param resolvedOrders The orders to fill
     /// @param filler This filler must be `whitelistedCaller`
     /// @param fillData It has the below encoded:
-    /// address[] memory tokensToApproveForSwapRouter02: Max approve these tokens to the 1 inch contract
+    /// address[] memory tokensToApproveForAggregator: Max approve these tokens to the 1 inch contract
     /// address[] memory tokensToApproveForReactor: Max approve these tokens to reactor
-    /// bytes[] memory multicallData: Pass into swapRouter02.multicall()
+    /// bytes memory swapData: Calldata for the aggregator.unoswap() function
     function reactorCallback(ResolvedOrder[] calldata resolvedOrders, address filler, bytes calldata fillData)
         external
     {
@@ -55,12 +61,13 @@ contract AggregatorExecutor is IReactorCallback, Owned {
             bytes memory swapData
         ) = abi.decode(fillData, (address[], address[], bytes));
 
-        for (uint256 i = 0; i < tokensToApproveForAggregator.length; i++) {
-            ERC20(tokensToApproveForAggregator[i]).approve(aggregator, type(uint256).max);
-        }
-
-        for (uint256 i = 0; i < tokensToApproveForReactor.length; i++) {
-            ERC20(tokensToApproveForReactor[i]).approve(reactor, type(uint256).max);
+        unchecked {
+            for (uint256 i = 0; i < tokensToApproveForAggregator.length; i++) {
+                ERC20(tokensToApproveForAggregator[i]).approve(aggregator, type(uint256).max);
+            }
+            for (uint256 i = 0; i < tokensToApproveForReactor.length; i++) {
+                ERC20(tokensToApproveForReactor[i]).approve(reactor, type(uint256).max);
+            }
         }
 
         (bool success,) = aggregator.call(swapData);
@@ -72,16 +79,30 @@ contract AggregatorExecutor is IReactorCallback, Owned {
     /// @param tokensToApprove Max approve these tokens to swapRouter02
     /// @param multicallData Pass into swapRouter02.multicall()
     function multicall(address[] calldata tokensToApprove, bytes[] calldata multicallData) external onlyOwner {
-        for (uint256 i = 0; i < tokensToApprove.length; i++) {
-            ERC20(tokensToApprove[i]).approve(swapRouter02, type(uint256).max);
+        unchecked {
+            for (uint256 i = 0; i < tokensToApprove.length; i++) {
+                ERC20(tokensToApprove[i]).approve(address(SWAP_ROUTER_02), type(uint256).max);
+            }
         }
-        ISwapRouter02(swapRouter02).multicall(type(uint256).max, multicallData);
+
+        SWAP_ROUTER_02.multicall(type(uint256).max, multicallData);
     }
 
     /// @notice Transfer all ETH in this contract to the recipient. Can only be called by owner.
     /// @param recipient The recipient of the ETH
     function withdrawETH(address recipient) external onlyOwner {
         SafeTransferLib.safeTransferETH(recipient, address(this).balance);
+    }
+
+    /// @notice Unwraps WETH to ETH and sends ETH to recipient.
+    /// @param recipient The recipient of the ETH.
+    function unwrapWETH(address recipient) external onlyOwner {
+        uint256 balance = WETH9.balanceOf(address(this));
+
+        if (balance == 0) revert InsufficientWETHBalance();
+
+        WETH9.withdraw(balance);
+        SafeTransferLib.safeTransferETH(recipient, balance);
     }
 
     receive() external payable {}
