5154b88a40449725af58fa988ce4c7cd1624e96a
Allen Lin <allenlin1992@hotmail.com>
2023-04-11 11:30:25 -0400

    update solidity version
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- test/util/InvariantTest.sol
+++ test/util/InvariantTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // from: https://github.com/maple-labs/contract-test-utils/blob/add-bounded-invariants/contracts/test.sol
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
 contract InvariantTest {
     address[] private _excludedContracts;
