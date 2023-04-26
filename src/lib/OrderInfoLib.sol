f2799c34a89446297e5513f701b6ab709534ae15
marktoda <toda.mark@gmail.com>
2023-04-26 07:54:19 -0700

    feat: hash orderinfo separately (#135)
    * feat: hash orderinfo separately
    
    Avoids stack too deep without bricking EIP712 and cleaner
    
    * fix: struct ordering
    
    * fix: alice comments
    
    * feat: add comment

---
+++ src/lib/OrderInfoLib.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.19;
+
+import {OrderInfo} from "../base/ReactorStructs.sol";
+
+/// @notice helpers for handling OrderInfo objects
+library OrderInfoLib {
+    bytes internal constant ORDER_INFO_TYPE =
+        "OrderInfo(address reactor,address offerer,uint256 nonce,uint256 deadline,address validationContract,bytes validationData)";
+    bytes32 internal constant ORDER_INFO_TYPE_HASH = keccak256(ORDER_INFO_TYPE);
+
+    function hash(OrderInfo memory info) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                ORDER_INFO_TYPE_HASH,
+                info.reactor,
+                info.offerer,
+                info.nonce,
+                info.deadline,
+                info.validationContract,
+                keccak256(info.validationData)
+            )
+        );
+    }
+}
