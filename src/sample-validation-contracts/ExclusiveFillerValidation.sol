dfa6ffe14afa4dd4966bea65f6b5309805d4ba06
Allen Lin <allenlin1992@hotmail.com>
2023-05-04 15:29:39 -0400

    deleted the constructor
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- src/sample-validation-contracts/ExclusiveFillerValidation.sol
+++ src/sample-validation-contracts/ExclusiveFillerValidation.sol
@@ -5,8 +5,6 @@ import {IValidationCallback} from "../interfaces/IValidationCallback.sol";
 import {ResolvedOrder, OrderInfo} from "../base/ReactorStructs.sol";
 
 contract ExclusiveFillerValidation is IValidationCallback {
-    constructor() {}
-
     function validate(address filler, ResolvedOrder calldata resolvedOrder) external view returns (bool) {
         (address exclusiveFiller, uint256 lastExclusiveTimestamp) =
             abi.decode(resolvedOrder.info.validationData, (address, uint256));
