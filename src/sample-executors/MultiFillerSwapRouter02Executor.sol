7814aeb7776ce405075e87efdb17a25f61deb689
Eric Zhong <eric.zhong@uniswap.org>
2023-05-30 19:05:16 -0400

    Add whitelistedCaller mapping

---
+++ src/sample-executors/MultiFillerSwapRouter02Executor.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.19;
+
+import {Owned} from "solmate/src/auth/Owned.sol";
+import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {WETH} from "solmate/src/tokens/WETH.sol";
+import {IReactorCallback} from "../interfaces/IReactorCallback.sol";
+import {CurrencyLibrary} from "../lib/CurrencyLibrary.sol";
+import {ResolvedOrder, OutputToken} from "../base/ReactorStructs.sol";
+import {ISwapRouter02} from "../external/ISwapRouter02.sol";
+
+/// @notice A fill contract that uses SwapRouter02 to execute trades
+contract SwapRouter02Executor is IReactorCallback, Owned {
+    using SafeTransferLib for ERC20;
+    using CurrencyLibrary for address;
+
+    error CallerNotWhitelisted();
+    error MsgSenderNotReactor();
+    error EtherSendFail();
+    error InsufficientWETHBalance();
+
+    address private immutable swapRouter02;
+    address private immutable reactor;
+    WETH private immutable weth;
+
+    mapping(address => bool) whitelistedCallers;
+
+    constructor(address _reactor, address _owner, address _swapRouter02) Owned(_owner) {
+        reactor = _reactor;
+        swapRouter02 = _swapRouter02;
+        weth = WETH(payable(ISwapRouter02(_swapRouter02).WETH9()));
+    }
+
+    function addWhitelistedCaller(address _whitelistedCaller) onlyOwner() external {
+        whitelistedCallers[_whitelistedCaller] = true;
+    }
+
+    /// @param resolvedOrders The orders to fill
+    /// @param filler This filler must be `whitelistedCaller`
+    /// @param fillData It has the below encoded:
+    /// address[] memory tokensToApproveForSwapRouter02: Max approve these tokens to swapRouter02
+    /// address[] memory tokensToApproveForReactor: Max approve these tokens to reactor
+    /// bytes[] memory multicallData: Pass into swapRouter02.multicall()
+    function reactorCallback(ResolvedOrder[] calldata resolvedOrders, address filler, bytes calldata fillData)
+        external
+    {
+        if (msg.sender != reactor) {
+            revert MsgSenderNotReactor();
+        }
+        if (!whitelistedCallers[filler]) {
+            revert CallerNotWhitelisted();
+        }
+
+        (address[] memory tokensToApproveForSwapRouter02, bytes[] memory multicallData) =
+            abi.decode(fillData, (address[], bytes[]));
+
+        for (uint256 i = 0; i < tokensToApproveForSwapRouter02.length; i++) {
+            ERC20(tokensToApproveForSwapRouter02[i]).safeApprove(swapRouter02, type(uint256).max);
+        }
+
+        ISwapRouter02(swapRouter02).multicall(type(uint256).max, multicallData);
+
+        for (uint256 i = 0; i < resolvedOrders.length; i++) {
+            ResolvedOrder memory order = resolvedOrders[i];
+            for (uint256 j = 0; j < order.outputs.length; j++) {
+                OutputToken memory output = order.outputs[j];
+                output.token.transfer(output.recipient, output.amount);
+            }
+        }
+    }
+
+    /// @notice This function can be used to convert ERC20s to ETH that remains in this contract
+    /// @param tokensToApprove Max approve these tokens to swapRouter02
+    /// @param multicallData Pass into swapRouter02.multicall()
+    function multicall(address[] calldata tokensToApprove, bytes[] calldata multicallData) external onlyOwner {
+        for (uint256 i = 0; i < tokensToApprove.length; i++) {
+            ERC20(tokensToApprove[i]).approve(swapRouter02, type(uint256).max);
+        }
+        ISwapRouter02(swapRouter02).multicall(type(uint256).max, multicallData);
+    }
+
+    /// @notice Unwraps the contract's WETH9 balance and sends it to the recipient as ETH. Can only be called by owner.
+    /// @param recipient The address receiving ETH
+    function unwrapWETH(address recipient) external onlyOwner {
+        uint256 balanceWETH = weth.balanceOf(address(this));
+
+        if (balanceWETH == 0) revert InsufficientWETHBalance();
+
+        weth.withdraw(balanceWETH);
+        SafeTransferLib.safeTransferETH(recipient, address(this).balance);
+    }
+
+    /// @notice Transfer all ETH in this contract to the recipient. Can only be called by owner.
+    /// @param recipient The recipient of the ETH
+    function withdrawETH(address recipient) external onlyOwner {
+        SafeTransferLib.safeTransferETH(recipient, address(this).balance);
+    }
+
+    receive() external payable {}
+}
