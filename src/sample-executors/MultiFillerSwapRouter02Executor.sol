08220236391881c96ab0bea2ffd55f4b86b1d1a6
Eric Zhong <eric.zhong@uniswap.org>
2023-05-30 19:10:16 -0400

    Change contract name

--- src/sample-executors/MultiFillerSwapRouter02Executor.sol
+++ src/sample-executors/MultiFillerSwapRouter02Executor.sol
@@ -13,7 +13,7 @@ import {ISwapRouter02} from "../external/ISwapRouter02.sol";
 /// @notice A fill contract that uses SwapRouter02 to execute trades
 /// @dev This is the same functionality as SwapRouter02Executor, but it allows the owner
 ///      to whitelist multiple fillers to execute trades.
-contract SwapRouter02Executor is IReactorCallback, Owned {
+contract MultiFillerSwapRouter02Executor is IReactorCallback, Owned {
     using SafeTransferLib for ERC20;
     using CurrencyLibrary for address;
 
