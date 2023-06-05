87a65476a0f15a4cbe9c9c3467b40bc74d4016cd
AzFlin <allenlin1992@hotmail.com>
2023-06-05 13:03:20 -0400

    Improve process direct fill (#149)
    * remove unnecessary check from _processDirectFill because reactor will no longer custody ETH
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * write test3OrdersWithEthAndERC20OutputsDirectFill
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    * write test2EthOutputOrdersButOnlyEnoughEthToFill1
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>
    
    ---------
    
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- src/lib/CurrencyLibrary.sol
+++ src/lib/CurrencyLibrary.sol
@@ -15,7 +15,6 @@ library CurrencyLibrary {
 
     /// @notice Thrown when a native transfer fails
     error NativeTransferFailed();
-    error NotEnoughETHDirectFiller();
 
     /// @notice Get the balance of a currency for addr
     /// @param currency The currency to get the balance of
@@ -53,7 +52,6 @@ library CurrencyLibrary {
         internal
     {
         if (isNative(currency)) {
-            if (msg.value < amount) revert NotEnoughETHDirectFiller();
             (bool success,) = recipient.call{value: amount}("");
             if (!success) revert NativeTransferFailed();
         } else {
