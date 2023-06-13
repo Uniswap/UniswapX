771c45ec4603919bcacda797db44c9eb08fd1783
Allen Lin <allenlin1992@hotmail.com>
2023-06-13 16:16:45 -0400

    add constructor comment, CVF 22
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

--- src/interfaces/IProtocolFeeController.sol
+++ src/interfaces/IProtocolFeeController.sol
@@ -6,7 +6,7 @@ import {ResolvedOrder, OutputToken} from "../base/ReactorStructs.sol";
 /// @notice Interface for getting fee outputs
 interface IProtocolFeeController {
     /// @notice Get fee outputs for the given orders
-    /// @param orders The orders to get fee outputs for
+    /// @param order The orders to get fee outputs for
     /// @return List of fee outputs to append for each provided order
     function getFeeOutputs(ResolvedOrder memory order) external view returns (OutputToken[] memory);
 }
