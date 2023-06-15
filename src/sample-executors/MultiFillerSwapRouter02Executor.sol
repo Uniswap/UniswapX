1dd18f617f4deb40c73a6b336724fc5fa9416631
Eric Zhong <eric.zhong@uniswap.org>
2023-06-15 12:47:12 -0400

    save state

--- src/sample-executors/MultiFillerSwapRouter02Executor.sol
+++ src/sample-executors/MultiFillerSwapRouter02Executor.sol
@@ -10,10 +10,138 @@ import {CurrencyLibrary} from "../lib/CurrencyLibrary.sol";
 import {ResolvedOrder, OutputToken} from "../base/ReactorStructs.sol";
 import {ISwapRouter02} from "../external/ISwapRouter02.sol";
 
+contract WhitelistedFillerStorage {
+    address private immutable f1;
+    address private immutable f2;
+    address private immutable f3;
+    address private immutable f4;
+    address private immutable f5;
+    address private immutable f6;
+    address private immutable f7;
+    address private immutable f8;
+    address private immutable f9;
+    address private immutable f10;
+
+    constructor(
+        address _f1,
+        address _f2,
+        address _f3,
+        address _f4,
+        address _f5,
+        address _f6,
+        address _f7,
+        address _f8,
+        address _f9,
+        address _f10
+    ) {
+        f1 = _f1;
+        f2 = _f2;
+        f3 = _f3;
+        f4 = _f4;
+        f5 = _f5;
+        f6 = _f6;
+        f7 = _f7;
+        f8 = _f8;
+        f9 = _f9;
+        f10 = _f10;
+    }
+
+    function isWhitelistedFiller(address caller) internal view returns (bool) {
+        // if (caller == f1 || caller == f2 || caller == f3 || caller == f4 || caller == f5 ||
+        //     caller == f6 || caller == f7 || caller == f8 || caller == f9 || caller == f10) {
+        //     return true;
+        // }
+        
+        return false;
+    }
+
+    // function isWhitelistedFiller(address caller) internal view returns (bool) {
+    //     // binary search, addresses are sorted in descending order
+    //     unchecked {
+    //         if (f5 == caller) return true;
+    //         if (f5 < caller) {
+    //             if (f3 == caller) return true;
+    //             if (f3 < caller) {
+    //                 if (f2 == caller) return true;
+    //                 if (f2 < caller) {
+    //                     if (f1 == caller) return true;
+    //                     if (f1 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f4 == caller;
+    //                     }
+    //                 } else {
+    //                     if (f4 == caller) return true;
+    //                     if (f4 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f2 == caller;
+    //                     }
+    //                 }
+    //             } else {
+    //                 if (f4 == caller) return true;
+    //                 if (f4 < caller) {
+    //                     if (f6 == caller) return true;
+    //                     if (f6 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f3 == caller;
+    //                     }
+    //                 } else {
+    //                     if (f3 == caller) return true;
+    //                     if (f3 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f6 == caller;
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             if (f7 == caller) return true;
+    //             if (f7 < caller) {
+    //                 if (f6 == caller) return true;
+    //                 if (f6 < caller) {
+    //                     if (f9 == caller) return true;
+    //                     if (f9 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f8 == caller;
+    //                     }
+    //                 } else {
+    //                     if (f8 == caller) return true;
+    //                     if (f8 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f6 == caller;
+    //                     }
+    //                 }
+    //             } else {
+    //                 if (f8 == caller) return true;
+    //                 if (f8 < caller) {
+    //                     if (f10 == caller) return true;
+    //                     if (f10 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f7 == caller;
+    //                     }
+    //                 } else {
+    //                     if (f7 == caller) return true;
+    //                     if (f7 < caller) {
+    //                         return false;
+    //                     } else {
+    //                         return f10 == caller;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
+
 /// @notice A fill contract that uses SwapRouter02 to execute trades
 /// @dev This is the same functionality as SwapRouter02Executor, but it allows the owner
 ///      to whitelist multiple fillers to execute trades.
-contract MultiFillerSwapRouter02Executor is IReactorCallback, Owned {
+contract MultiFillerSwapRouter02Executor is IReactorCallback, WhitelistedFillerStorage, Owned {
     using SafeTransferLib for ERC20;
     using CurrencyLibrary for address;
 
@@ -26,22 +154,32 @@ contract MultiFillerSwapRouter02Executor is IReactorCallback, Owned {
     address private immutable reactor;
     WETH private immutable weth;
 
-    mapping(address => bool) whitelistedCallers;
-
-    constructor(address _reactor, address _owner, address _swapRouter02) Owned(_owner) {
+    // @param _reactor The address of the reactor contract
+    // @param _owner The owner of this contract
+    // @param _swapRouter02 The address of the SwapRouter02 contract
+    // This contract supports up to a maximum of 10 whitelisted fillers
+    // the addresses MUST be set in descending order
+    // if you want to set less than the maximum number of fillers, set the rest to 0x0
+    // @param _f1 The first whitelisted filler
+    // @param _f2 The second whitelisted filler
+    constructor(
+        address _reactor,
+        address _owner,
+        address _swapRouter02,
+        address _f1,
+        address _f2,
+        address _f3,
+        address _f4,
+        address _f5,
+        address _f6,
+        address _f7,
+        address _f8,
+        address _f9,
+        address _f10
+    ) Owned(_owner) WhitelistedFillerStorage(_f1, _f2, _f3, _f4, _f5, _f6, _f7, _f8, _f9, _f10) {
         reactor = _reactor;
         swapRouter02 = _swapRouter02;
         weth = WETH(payable(ISwapRouter02(_swapRouter02).WETH9()));
-    }
-
-    /// @param _whitelistedCaller The filler address to whitelist
-    function addWhitelistedCaller(address _whitelistedCaller) external onlyOwner {
-        whitelistedCallers[_whitelistedCaller] = true;
-    }
-
-    /// @param _whitelistedCaller The filler address to remove from whitelist
-    function removeWhitelistedCaller(address _whitelistedCaller) external onlyOwner {
-        whitelistedCallers[_whitelistedCaller] = false;
     }
 
     /// @param resolvedOrders The orders to fill
@@ -56,7 +194,7 @@ contract MultiFillerSwapRouter02Executor is IReactorCallback, Owned {
         if (msg.sender != reactor) {
             revert MsgSenderNotReactor();
         }
-        if (!whitelistedCallers[filler]) {
+        if (!isWhitelistedFiller(filler)) {
             revert CallerNotWhitelisted();
         }
 
