7b66edd0d2eb0462a11bc5c65964386f07711b3c
Mark Toda <toda.mark@gmail.com>
2023-07-25 01:02:34 -0700

    fix: natspec

--- src/lib/FillDataLib.sol
+++ src/lib/FillDataLib.sol
@@ -6,7 +6,7 @@ library FillDataLib {
     /// @dev special fillData used to indicate that the reactor callback should be skipped
     bytes1 constant SKIP_REACTOR_CALLBACK = 0x00;
 
-    /// @notice return whether or not the fillData represents a direct fill
+    /// @notice return whether or not the fillData specifies that reactorCallback should be called
     /// @param fillData The fill data to check
     function executeReactorCallback(bytes calldata fillData) internal pure returns (bool) {
         return fillData.length != 1 || fillData[0] != SKIP_REACTOR_CALLBACK;
