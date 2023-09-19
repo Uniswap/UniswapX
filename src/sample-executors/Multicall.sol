af93887d3c710489d0a0dfd92bea29b85a655390
Eric Zhong <eric.zhong@uniswap.org>
2023-09-19 14:09:10 -0400

    forge fmt

--- src/sample-executors/Multicall.sol
+++ src/sample-executors/Multicall.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.16;
 /// @notice Code from https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol
 /// @notice Enables calling multiple methods in a single call to the contract
 abstract contract Multicall {
-    function multicall(bytes[] calldata data) public virtual payable returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) public payable virtual returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
             (bool success, bytes memory result) = address(this).delegatecall(data[i]);
