b9c3d3757f3dc3c467acb719e43ef6b9f57317ba
Mark Toda <toda.mark@gmail.com>
2024-04-05 15:41:09 -0400

    feat: remove input and output decay validation
    Unnecessary restriction causing extra gas via looping through all
    outputs and checking if decaing

--- test/reactors/BaseDutchOrderReactor.t.sol
+++ test/reactors/BaseDutchOrderReactor.t.sol
@@ -349,27 +349,6 @@ abstract contract BaseDutchOrderReactorTest is PermitSignature, DeployPermit2, B
         quoter.quote(order.order, order.sig);
     }
 
-    // The input decays, which means the outputs must not decay. In this test, the
-    // 2nd output decays, so revert with error InputAndOutputDecay().
-    function test_dutch_validateInputAndOutputDecay() public {
-        uint256 currentTime = 100;
-
-        SignedOrder memory order = generateOrder(
-            TestDutchOrderSpec({
-                currentTime: currentTime,
-                startTime: currentTime,
-                endTime: currentTime + 100,
-                deadline: currentTime + 100,
-                input: DutchInput(tokenIn, 100, 110),
-                outputs: OutputsBuilder.multipleDutch(
-                    tokenOut, Solarray.uint256s(1000, 1000), Solarray.uint256s(1000, 900), address(0)
-                )
-            })
-        );
-        vm.expectRevert(DutchOrderReactor.InputAndOutputDecay.selector);
-        quoter.quote(order.order, order.sig);
-    }
-
     function test_dutch_validateInputDecayIncorrectAmounts() public {
         uint256 currentTime = 100;
 
