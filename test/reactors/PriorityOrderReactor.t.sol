e7dbca3879d6084c7ce4be93056f81436b33d7e8
Eric Zhong <eric.zhong@uniswap.org>
2024-07-18 10:04:12 -0400

    rm console2

--- test/reactors/PriorityOrderReactor.t.sol
+++ test/reactors/PriorityOrderReactor.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import {console2} from "forge-std/console2.sol";
 import {Test} from "forge-std/Test.sol";
 import {OrderInfo, InputToken, OutputToken, ResolvedOrder, SignedOrder} from "../../src/base/ReactorStructs.sol";
 import {ReactorEvents} from "../../src/base/ReactorEvents.sol";
