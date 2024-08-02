667dacfa96b534b1af28c9894810c98fb283747d
Eric Zhong <eric.zhong@uniswap.org>
2024-08-02 14:16:34 -0400

    Add deploy script and test

---
+++ script/DeployPriorityOrderReactor.s.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Script.sol";
+import {IPermit2} from "permit2/src/interfaces/IPermit2.sol";
+import {PriorityOrderReactor} from "../src/reactors/PriorityOrderReactor.sol";
+import {OrderQuoter} from "../src/lens/OrderQuoter.sol";
+import {DeployPermit2} from "../test/util/DeployPermit2.sol";
+
+struct PriorityOrderReactorDeployment {
+    IPermit2 permit2;
+    PriorityOrderReactor reactor;
+    OrderQuoter quoter;
+}
+
+contract DeployPriorityOrderReactor is Script, DeployPermit2 {
+    address constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address constant UNI_TIMELOCK = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
+
+    function setUp() public {}
+
+    function run() public returns (PriorityOrderReactorDeployment memory deployment) {
+        vm.startBroadcast();
+        if (PERMIT2.code.length == 0) {
+            deployPermit2();
+        }
+
+        PriorityOrderReactor reactor = new PriorityOrderReactor{salt: 0x00}(IPermit2(PERMIT2), UNI_TIMELOCK);
+        console2.log("Reactor", address(reactor));
+
+        OrderQuoter quoter = new OrderQuoter{salt: 0x00}();
+        console2.log("Quoter", address(quoter));
+
+        vm.stopBroadcast();
+
+        return PriorityOrderReactorDeployment(IPermit2(PERMIT2), reactor, quoter);
+    }
+}
