8d03b1bb09b20edf5e84ff79c2da08639e477d0f
Eric Zhong <eric.zhong@uniswap.org>
2024-10-03 11:43:46 -0400

    Move MockArbSys to test/util/mock/

--- test/base/BlockNumberish.t.sol
+++ test/base/BlockNumberish.t.sol
@@ -1,23 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.0;
-
 import {Test} from "forge-std/Test.sol";
+import {MockArbSys} from "../util/mock/MockArbSys.sol";
 import {BlockNumberish} from "../../src/base/BlockNumberish.sol";
-
-/// Mock Arbitrum syscall contract
-contract MockArbSys {
-    uint256 _blockNumber;
-
-    /// @dev helper function to set the block number
-    function setBlockNumber(uint256 blockNumber) external {
-        _blockNumber = blockNumber;
-    }
-
-    /// @notice returns the block number
-    function arbBlockNumber() external view returns (uint256) {
-        return _blockNumber;
-    }
-}
 
 contract MockBlockNumberish is BlockNumberish {
     function getBlockNumberish() external view returns (uint256) {
