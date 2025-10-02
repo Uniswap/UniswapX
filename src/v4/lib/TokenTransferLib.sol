4dfec96bf595e5df15f8d5e33611211d01002e05
Alan Wu <alanwu100@gmail.com>
2025-10-01 22:45:23 -0400

    feat: add AllowanceTransfer functions to transfer library

--- src/v4/lib/TokenTransferLib.sol
+++ src/v4/lib/TokenTransferLib.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {IPermit2} from "permit2/src/interfaces/IPermit2.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
 import {ResolvedOrder} from "../base/ReactorStructs.sol";
 import {Permit2Lib} from "./Permit2Lib.sol";
 import {IAuctionResolver} from "../interfaces/IAuctionResolver.sol";
@@ -21,6 +22,39 @@ library TokenTransferLib {
         // Execute the token transfer via Permit2
         permit2.permitWitnessTransferFrom(
             order.toPermit(), order.transferDetails(to), order.info.swapper, order.hash, orderType, order.sig
+        );
+    }
+
+    /// @notice Set allowance for tokens using permit signature
+    /// @dev Hook should call this first if a permit signature is provided
+    /// @param permit2 The Permit2 contract instance
+    /// @param owner The token owner granting the allowance
+    /// @param permitSingle The permit details including spender, amount, expiration
+    /// @param signature The signature over the permit data
+    function setAllowance(
+        IPermit2 permit2,
+        address owner,
+        IAllowanceTransfer.PermitSingle calldata permitSingle,
+        bytes calldata signature
+    ) internal {
+        permit2.permit(owner, permitSingle, signature);
+    }
+
+    /// @notice Transfer input tokens using existing allowance
+    /// @dev Assumes allowance has been set (either via setAllowance or previously)
+    /// @param permit2 The Permit2 contract instance
+    /// @param order The resolved order containing transfer details
+    /// @param to The recipient address (typically the filler)
+    function allowanceTransferInputTokens(
+        IPermit2 permit2,
+        ResolvedOrder calldata order,
+        address to
+    ) internal {
+        permit2.transferFrom(
+            order.info.swapper,
+            to,
+            uint160(order.input.amount),
+            order.input.token
         );
     }
 }
