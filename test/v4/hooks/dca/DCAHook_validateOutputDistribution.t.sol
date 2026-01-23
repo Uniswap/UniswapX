734c588c446ded6e4993e3e035657648e1db6f9f
Alan Wu <alanwu100@gmail.com>
2026-01-23 11:47:19 -0500

    test: exactIn sweep rounding remainder

--- test/v4/hooks/dca/DCAHook_validateOutputDistribution.t.sol
+++ test/v4/hooks/dca/DCAHook_validateOutputDistribution.t.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.8.0;
 import {Test} from "forge-std/Test.sol";
 import {IPermit2} from "permit2/src/interfaces/IPermit2.sol";
 import {DeployPermit2} from "../../../util/DeployPermit2.sol";
+
 import {DCAHookHarness} from "./DCAHookHarness.sol";
 import {IReactor} from "../../../../src/v4/interfaces/IReactor.sol";
-import {
-    DCAIntent,
-    DCAOrderCosignerData,
-    OutputAllocation,
-    PrivateIntent,
-    FeedInfo
-} from "../../../../src/v4/hooks/dca/DCAStructs.sol";
+
+import {DCAIntent, DCAOrderCosignerData, OutputAllocation, PrivateIntent, FeedInfo} from "../../../../src/v4/hooks/dca/DCAStructs.sol";
 import {OutputToken} from "../../../../src/base/ReactorStructs.sol";
 import {IDCAHook} from "../../../../src/v4/interfaces/IDCAHook.sol";
 
 contract DCAHook_validateOutputDistributionTest is Test, DeployPermit2 {
     DCAHookHarness hook;
     IPermit2 permit2;
+
     address constant REACTOR_ADDRESS = address(0x2345);
     IReactor constant REACTOR = IReactor(REACTOR_ADDRESS);
 
@@ -62,7 +59,7 @@ contract DCAHook_validateOutputDistributionTest is Test, DeployPermit2 {
         });
     }
 
-    function test_validateOutputDistribution_exactOut_remainderAssignedToFirstMaxBpsRecipient() public {
+    function test_validateOutputDistribution_exactOut_remainderAssignedToFirstMaxBpsRecipient() public view {
         DCAIntent memory intent = _createExactOutIntent();
         DCAOrderCosignerData memory cosignerData = hook.createTestCosignerData(SWAPPER, NONCE, 101, 0, 0);
 
@@ -85,5 +82,34 @@ contract DCAHook_validateOutputDistributionTest is Test, DeployPermit2 {
 
         vm.expectRevert(abi.encodeWithSelector(IDCAHook.AllocationMismatch.selector, RECIPIENT_A, 50, 51));
         hook.validateOutputDistribution(intent, cosignerData, outputs);
+    }
+
+    /// For EXACT_IN, expected allocations are floored and checked per-recipient with ±1 tolerance,
+    /// but there's no requirement that *all* output goes only to allocation recipients.
+    /// A remainder can be swept to an unallocated recipient without reverting.
+    function test_validateOutputDistribution_exactIn_allowsUnallocatedRemainderSweep_shouldRevert() public {
+        address attacker = address(0xBAD); // NOT allocated
+
+        OutputAllocation[] memory allocs = new OutputAllocation[](2);
+        allocs[0] = OutputAllocation({recipient: RECIPIENT_A, basisPoints: 5000});
+        allocs[1] = OutputAllocation({recipient: RECIPIENT_B, basisPoints: 5000});
+
+        DCAIntent memory intent;
+        intent.isExactIn = true;
+        intent.outputAllocations = allocs;
+
+        // totalOutput = 3
+        // expected floors are 1 and 1; the "extra" 1 can be sent to attacker.
+        OutputToken[] memory outputs = new OutputToken[](3);
+        outputs[0] = OutputToken({token: address(0xBEEF), amount: 1, recipient: RECIPIENT_A});
+        outputs[1] = OutputToken({token: address(0xBEEF), amount: 1, recipient: RECIPIENT_B});
+        outputs[2] = OutputToken({token: address(0xBEEF), amount: 1, recipient: attacker});
+
+        DCAOrderCosignerData memory cd;
+        cd.limitAmount = 0; // don't fail on totalOutput < limit
+
+        // Correct behavior would revert
+        vm.expectRevert();
+        hook.validateOutputDistribution(intent, cd, outputs);
     }
 }
